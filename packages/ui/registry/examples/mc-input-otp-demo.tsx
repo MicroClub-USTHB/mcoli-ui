9619d5a3bc1291418dfb663f108a6cd2b1d0c01c
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-30 20:04:18 +0100

    Add demo component for mc-input-otp

---
+++ packages/ui/registry/examples/mc-input-otp-demo.tsx
@@ -0,0 +1,25 @@
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+  InputOTPSeparator,
+} from "../ui/mc-input-otp";
+
+export default function McInputOtpDemo() {
+  return (
+    <InputOTP maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={3} />
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
+  );
+}
