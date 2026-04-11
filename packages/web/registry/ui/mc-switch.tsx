fdd15c468aab21006d978d930263204d67c97659
Adel HB <hadjarabadel.2411@gmail.com>
2026-04-11 01:50:08 +0100

    style(mc-switch): changed sizes to be more realistic

--- packages/web/registry/ui/mc-switch.tsx
+++ packages/web/registry/ui/mc-switch.tsx
@@ -21,10 +21,10 @@ function McSwitch({
         'aria-invalid:ring-destructive aria-invalid:ring-offset-2',
         'dark:aria-invalid:ring-destructive/40',
         'data-disabled:bg-input/70 data-disabled:opacity-50',
-        'data-checked:bg-primary data-checked:ring-border',
+        'data-checked:bg-primary data-checked:ring-transparent',
         'data-unchecked:bg-input data-unchecked:ring-border',
-        'data-[size=default]:h-[72px] data-[size=default]:w-[132px] data-[size=default]:rounded-[42px] data-[size=default]:p-1',
-        'data-[size=sm]:h-[18.4px] data-[size=sm]:w-8 data-[size=sm]:rounded-full data-[size=sm]:p-0.5',
+        'data-[size=default]:h-7 data-[size=default]:w-12 data-[size=default]:rounded-full data-[size=default]:p-1',
+        'data-[size=sm]:h-4 data-[size=sm]:w-7 data-[size=sm]:rounded-full data-[size=sm]:p-[2px]',
         className
       )}
       {...props}
@@ -34,12 +34,12 @@ function McSwitch({
         className={cn(
           'pointer-events-none block rounded-full bg-background ring-1 ring-inset ring-border transition-transform duration-300 ease-out',
           'dark:data-checked:bg-primary-foreground dark:data-unchecked:bg-foreground',
-          'group-data-[size=default]/switch:h-[64px] group-data-[size=default]/switch:w-[64px]',
+          'group-data-[size=default]/switch:size-5',
           'group-data-[size=default]/switch:data-unchecked:translate-x-0',
-          'group-data-[size=default]/switch:data-checked:translate-x-[60px]',
+          'group-data-[size=default]/switch:data-checked:translate-x-5',
           'group-data-[size=sm]/switch:size-3',
           'group-data-[size=sm]/switch:data-unchecked:translate-x-0',
-          'group-data-[size=sm]/switch:data-checked:translate-x-[calc(100%-2px)]'
+          'group-data-[size=sm]/switch:data-checked:translate-x-3'
         )}
       />
     </SwitchPrimitive.Root>
