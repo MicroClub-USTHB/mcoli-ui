8637cac9ceb2a8640f652f022422f19a190c413e
Adel HB <hadjarabadel.2411@gmail.com>
2026-04-11 01:50:28 +0100

    docs(mc-switch): implemented the playground story

--- packages/web/stories/McSwitch.stories.tsx
+++ packages/web/stories/McSwitch.stories.tsx
@@ -4,9 +4,20 @@ import { McSwitch } from '@/registry/ui/mc-switch';
 const meta: Meta<typeof McSwitch> = {
   title: 'Components/McSwitch',
   component: McSwitch,
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['default', 'sm'],
+    },
+  },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof McSwitch>;
 
-export const Default: Story = {};
+export const Playground: Story = {
+  args: {
+    size: 'default',
+  },
+};
