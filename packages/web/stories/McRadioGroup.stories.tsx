a2be0d7292b0d5be7a08242a3ce6d7e03b751d31
MrmAya <kahlimeriemaya@gmail.com>
2026-04-11 17:53:45 +0200

    feat(Radio Group): add Radio Group component and demo example

---
+++ packages/web/stories/McRadioGroup.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+const meta = {
+  title: 'Components/McRadioGroup',
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {};
