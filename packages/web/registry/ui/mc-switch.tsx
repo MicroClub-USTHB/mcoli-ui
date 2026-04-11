7c38f92d70cd6d356d1b74551f4aa7646711a747
Adel HB <hadjarabadel.2411@gmail.com>
2026-04-11 01:22:47 +0100

    fix(mc-switch): fixed inner-shadow and padding inconsistencies

--- packages/web/registry/ui/mc-switch.tsx
+++ packages/web/registry/ui/mc-switch.tsx
@@ -15,16 +15,15 @@ function McSwitch({
       data-slot="switch"
       data-size={size}
       className={cn(
-        'peer group/switch relative inline-flex shrink-0 items-center border-border bg-input border border-borde',
-        'transition-[background-color,border-color,box-shadow] duration-300 ease-out outline-none',
-        'focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50',
-        'aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20',
+        'peer group/switch relative inline-flex shrink-0 items-center bg-input ring-1 ring-inset ring-border',
+        'transition-[background-color,ring-color,box-shadow] duration-300 ease-out outline-none',
+        'focus-visible:ring-ring focus-visible:ring-offset-2',
+        'aria-invalid:ring-destructive aria-invalid:ring-offset-2',
         'dark:aria-invalid:ring-destructive/40',
-        'data-disabled:border-border data-disabled:bg-input/70',
-        'data-checked:bg-primary data-checked:border-border',
-        'data-unchecked:bg-input data-unchecked:border-border',
-        'data-[size=default]:h-18 data-[size=default]:w-33 data-[size=default]:rounded-[42px] data-[size=default]:py-1 data-[size=default]:pl-1 data-[size=default]:pr-4 data-[size=default]:gap-4',
-
+        'data-disabled:bg-input/70 data-disabled:opacity-50',
+        'data-checked:bg-primary data-checked:ring-border',
+        'data-unchecked:bg-input data-unchecked:ring-border',
+        'data-[size=default]:h-[72px] data-[size=default]:w-[132px] data-[size=default]:rounded-[42px] data-[size=default]:p-1',
         'data-[size=sm]:h-[18.4px] data-[size=sm]:w-8 data-[size=sm]:rounded-full data-[size=sm]:p-0.5',
         className
       )}
@@ -33,11 +32,11 @@ function McSwitch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          'pointer-events-none block rounded-full bg-background ring-0 transition-transform duration-300 ease-out',
+          'pointer-events-none block rounded-full bg-background ring-1 ring-inset ring-border transition-transform duration-300 ease-out',
           'dark:data-checked:bg-primary-foreground dark:data-unchecked:bg-foreground',
-          'group-data-[size=default]/switch:size-16',
-          'group-data-[size=default]/switch:data-unchecked:translate-x-0 ',
-          'group-data-[size=default]/switch:data-checked:translate-x-[calc(132px-64px-8px)] border',
+          'group-data-[size=default]/switch:h-[64px] group-data-[size=default]/switch:w-[64px]',
+          'group-data-[size=default]/switch:data-unchecked:translate-x-0',
+          'group-data-[size=default]/switch:data-checked:translate-x-[60px]',
           'group-data-[size=sm]/switch:size-3',
           'group-data-[size=sm]/switch:data-unchecked:translate-x-0',
           'group-data-[size=sm]/switch:data-checked:translate-x-[calc(100%-2px)]'
