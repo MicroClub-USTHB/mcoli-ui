2408ae37c6ec1c5916e9afb3d735b85285faa17a
ABDELALIM DIB <abdelalim.dib@etu.usthb.dz>
2026-04-11 22:38:55 +0100

    fix : fix all smal remark and what i should change

--- packages/web/components/mdx/component-preview.tsx
+++ packages/web/components/mdx/component-preview.tsx
@@ -7,9 +7,6 @@ import { Index } from '@/__registry__';
 export const ComponentPreview: React.FC<{ name: string }> = ({ name }) => {
   const Preview = React.useMemo(() => {
     const Component = Index[name]?.component;
-    if (!Component) {
-      return null;
-    }
     return <Component />;
   }, [name]);
   return (
