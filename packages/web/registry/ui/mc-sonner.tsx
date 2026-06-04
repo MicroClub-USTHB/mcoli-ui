fcd59f9ab6bfb6c4705fef8b84c22bcc890e1a5d
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-06-04 01:25:52 +0100

    feat: update component to match Figma specs

--- packages/web/registry/ui/mc-sonner.tsx
+++ packages/web/registry/ui/mc-sonner.tsx
@@ -13,25 +13,23 @@ const McSonner = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            'w-97.5 h-18.5 flex items-center justify-center gap-1.5 p-4 border rounded-lg shadow-xs ',
-          content: 'flex flex-col gap-1.5 justify-center',
-          title: 'text-[14px] font-semibold text-[#0F172A] leading-tight',
-          description: 'text-xs text-[#64748B] font-normal leading-normal',
-          actionButton:
-            'rounded-lg bg-[#0000BA] px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-900 focus:outline-none shrink-0',
+            'w-97.5 min-h-18.5 flex items-center justify-between gap-1.5 p-4 border rounded-lg shadow-xs ',
+          content: 'w-full h-full flex flex-col items-start justify-center gap-0.5',
+          title: 'text-sm text-foreground max-w-66.5',
+          description: 'text-sm text-muted-foreground max-w-66.5',
+          actionButton: 'min-w-15.5 max-w-21.5 rounded-lg bg-primary',
           icon: 'text-slate-600 shrink-0 h-5 w-5',
         },
       }}
-      // C'EST ICI qu'on associe les icônes aux méthodes de Sonner
       icons={{
         success: <CheckCircle2 className="text-slate-700" />,
-        info: <AlertCircle className="text-slate-700" />,
+        warning: <AlertCircle className="text-slate-700" />,
+        error: <AlertCircle className="text-slate-700" />,
       }}
       {...props}
     />
   );
 };
 
-// On ré-exporte la fonction 'toast' pour pouvoir l'appeler partout
 export { toast } from 'sonner';
 export default McSonner;
