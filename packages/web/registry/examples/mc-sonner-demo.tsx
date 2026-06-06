414f3a69915d88d4634e94c659211e3edf4396ae
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-06-06 02:07:46 +0100

    feat: update the  mc-sonner.mdx

--- packages/web/registry/examples/mc-sonner-demo.tsx
+++ packages/web/registry/examples/mc-sonner-demo.tsx
@@ -1,9 +1,8 @@
 'use client';
 import McSonner, { toast } from '../ui/mc-sonner';
-
 export default function McSonnerDemo() {
   return (
-    <div className="p-12 flex flex-col gap-4 items-center justify-center h-screen w-screen bg-slate-50">
+    <div className="p-12 flex flex-col gap-4 items-center justify-center">
       <button
         onClick={() =>
           toast('Event has been created', {
@@ -16,7 +15,7 @@ export default function McSonnerDemo() {
         }
         className="px-5 py-2.5 bg-slate-900 text-white rounded-lg text-sm font-medium transition-all shadow-sm hover:bg-slate-800"
       >
-        Show Simple Toast
+        Show Toast
       </button>
       <McSonner />
     </div>
