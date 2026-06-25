be7b984fa84564b41cffc60ff7e9a1203bbf2783
berkal749 <berkalachrefpro@gmail.com>
2026-06-25 13:38:05 +0100

    chore: added embla-carousel-react

--- packages/web/registry/registry-ui.ts
+++ packages/web/registry/registry-ui.ts
@@ -403,6 +403,6 @@ export const ui: Registry['items'] = [
         type: 'registry:component',
       },
     ],
-    dependencies: ['@base-ui/react'],
+    dependencies: ['@base-ui/react', 'embla-carousel-react', 'lucide-react'],
   },
 ];
