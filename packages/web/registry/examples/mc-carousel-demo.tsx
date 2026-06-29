78d7616413c4e98f7b8d4fe7a1a2b82bce2c76c8
berkal749 <berkalachrefpro@gmail.com>
2026-06-29 20:08:53 +0100

    feat:carsoule demo made

--- packages/web/registry/examples/mc-carousel-demo.tsx
+++ packages/web/registry/examples/mc-carousel-demo.tsx
@@ -1,32 +1,25 @@
-import * as React from 'react';
-
-import { Card, CardContent } from '@/components/ui/card';
 import {
-  McCarousel,
+  Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from '../ui/mc-carousel';
 
-export function CarouselDemo() {
+export default function McCarouselDemo() {
   return (
-    <McCarousel className="w-full max-w-[12rem] sm:max-w-xs">
+    <Carousel className="w-full max-w-xs">
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
+            <div className="flex aspect-square items-center justify-center p-6">
+              <span className="text-4xl font-semibold">{index + 1}</span>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
-    </McCarousel>
+    </Carousel>
   );
 }
