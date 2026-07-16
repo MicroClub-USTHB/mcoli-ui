97925ccad49de50a40206065b4d03d2b909c8c79
ABDELALIM DIB <abdelalim.dib@etu.usthb.dz>
2026-07-16 01:24:15 +0100

    fix():lint problem

--- packages/web/stories/McAccordion.stories.tsx
+++ packages/web/stories/McAccordion.stories.tsx
@@ -5,8 +5,6 @@ import {
   McAccordionItem,
   McAccordionTrigger,
 } from '@/registry/ui/mc-accordion';
-import { AccordionRoot } from '@base-ui/react';
-import { JSX } from 'react/jsx-runtime';
 
 const items = [
   {
@@ -48,7 +46,7 @@ export default meta;
 type Story = StoryObj<typeof McAccordion>;
 
 export const Playground: Story = {
-  render: (args: JSX.IntrinsicAttributes & AccordionRoot.Props<any>) => (
+  render: (args) => (
     <McAccordion {...args} className="max-w-lg">
       {items.map((item) => (
         <McAccordionItem key={item.value} value={item.value}>
@@ -68,7 +66,7 @@ export const AllCollapsed: Story = {
   args: {
     defaultValue: undefined,
   },
-  render: (args: JSX.IntrinsicAttributes & AccordionRoot.Props<any>) => (
+  render: (args) => (
     <McAccordion {...args} className="max-w-lg">
       {items.map((item) => (
         <McAccordionItem key={item.value} value={item.value}>
