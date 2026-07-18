1232f639e9fd2b72cb675b8d23d0dc7fd6be3f69
ABDELALIM DIB <abdelalim.dib@etu.usthb.dz>
2026-07-18 02:42:33 +0100

    finish(): add the value indicator

--- packages/web/registry/ui/mc-slider.tsx
+++ packages/web/registry/ui/mc-slider.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Slider as SliderPrimitive } from '@base-ui/react/slider';
 
 import { cn } from '@/lib/utils';
@@ -8,23 +9,39 @@ function McSlider({
   value,
   min = 0,
   max = 100,
+  showValue = true,
+  border = false,
+  unity,
+  onValueChange,
   ...props
-}: SliderPrimitive.Root.Props) {
-  const _values = Array.isArray(value)
-    ? value
-    : Array.isArray(defaultValue)
-      ? defaultValue
-      : [min, max];
+}: SliderPrimitive.Root.Props & {
+  showValue?: boolean;
+  border?: boolean;
+  unity?: string;
+}) {
+  const isControlled = value !== undefined;
+
+  const [internalValues, setInternalValues] = React.useState<number[]>(
+    Array.isArray(defaultValue) ? defaultValue : [min]
+  );
+
+  const values = isControlled ? (Array.isArray(value) ? value : [value]) : internalValues;
 
   return (
     <SliderPrimitive.Root
-      className={cn('data-horizontal:w-full data-vertical:h-full', className)}
+      className={cn('data-horizontal:w-full data-vertical:h-full mt-12', className)}
       data-slot="slider"
       defaultValue={defaultValue}
       value={value}
       min={min}
       max={max}
       thumbAlignment="edge"
+      onValueChange={(newValues, event) => {
+        if (!isControlled) {
+          setInternalValues(Array.isArray(newValues) ? newValues : [newValues]);
+        }
+        onValueChange?.(newValues, event);
+      }}
       {...props}
     >
       <SliderPrimitive.Control className="relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col">
@@ -37,15 +54,25 @@ function McSlider({
             className="bg-primary select-none data-horizontal:h-full data-vertical:w-full"
           />
         </SliderPrimitive.Track>
-        {Array.from({ length: _values.length }, (_, index) => (
+
+        {values.map((currentValue, index) => (
           <SliderPrimitive.Thumb
-            data-slot="slider-thumb"
             key={index}
-            className="
-            relative block size-3 shrink-0 rounded-full border-2 border-primary bg-muted ring-accent-foreground backdrop-blur-3xl
-            transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 
-            focus-visible:outline-hidden active:ring-3 active:bg-primary disabled:pointer-events-none disabled:opacity-50 disabled:bg-blue-500"
-          />
+            data-slot="slider-thumb"
+            className="group relative block size-3 shrink-0 rounded-full border-2 border-primary bg-muted ring-accent-foreground backdrop-blur-3xl transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 active:bg-primary disabled:pointer-events-none disabled:opacity-50 disabled:bg-blue-500"
+          >
+            {showValue && (
+              <div
+                className={cn(
+                  'pointer-events-none absolute bottom-full left-1/2 mb-3 -translate-x-1/2 whitespace-nowrap rounded-xl ',
+                  'px-3.5 py-2.5 gap-2.5  text-md text-foreground bg-transparent ',
+                  border && 'ring-1 ring-inset ring-border bg-muted'
+                )}
+              >
+                {currentValue} {unity}
+              </div>
+            )}
+          </SliderPrimitive.Thumb>
         ))}
       </SliderPrimitive.Control>
     </SliderPrimitive.Root>
